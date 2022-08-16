f55b044dfcc5ff82bda8a425374b5c9754891aff
dkyocn99 <111041278+dkyocn99@users.noreply.github.com>
2022-08-16 17:24:36 +0900

    D btable creation (#3)
    * http-practice1,2
    
    * DBtable-creation
    
    * static폴더에 cat 이미지 리팩토링

--- blog/.idea/sonarlint/issuestore/index.pb
+++ blog/.idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,6 @@
 
 d
 4src/test/java/com/cos/blog/BlogApplicationTests.java,a/d/ad1c346ab774a0d96c98a7032afc7859cd8cfa11
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -0,0 +1,66 @@
+package com.cos.blog.test;
+
+
+import org.springframework.web.bind.annotation.*;
+
+//사용자가 요청 -> 응답(html파일)
+//@Controller
+
+//사용자가 요청-> 응답(Data)
+@RestController
+public class HttpControllerTest {
+
+    private static final String TAG="HttpControllerTest: ";
+
+    @GetMapping("/http/lombok")
+    public String lombokTest() {
+        Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+
+
+        System.out.println(TAG+"getter : "+m.getUsername());
+        m.setUsername("cos");
+        System.out.println(TAG+"getter : "+m.getUsername());
+
+        return "lombok test 완료";
+    }
+
+
+    //http://localhost:8000/blog/http/get?id=1&username=ssar&password=1234&email=ssar@nate.com
+    //인터넷 브라우저 요청은 get만 된다.
+    //http://localhost:8000/blog/http/get
+    //select
+    @GetMapping("/http/get")
+    public String getTest(Member member) {
+
+        return "get요청: "+member.getId()+","+member.getUsername()+","+member.getPassword()+","+member.getEmail();
+    }
+
+    //MessageConverter(스프링부트)
+    //appllication/json
+    //insert
+    @PostMapping("/http/post")
+    public String postTest(@RequestBody Member member) {
+
+        return "post요청: "+member.getId()+","+member.getUsername()+","+member.getPassword()+","+member.getEmail();
+    }
+
+//    @PostMapping("/http/post")//text/plan
+//    public String postTest(@RequestBody String text) {
+//
+//        return "post요청: "+text;
+//    }
+
+    //update
+    @PutMapping("/http/put")
+    public String putTest(@RequestBody Member member) {
+
+        return "put요청: "+member.getId()+","+member.getUsername()+","+member.getPassword()+","+member.getEmail();
+    }
+
+    //delete
+    @DeleteMapping("/http/delete")
+    public String deleteTest(@RequestBody Member member) {
+
+        return "delete요청: "+member.getId()+","+member.getUsername()+","+member.getPassword()+","+member.getEmail();
+    }
+}
