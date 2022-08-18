c24f08fc26e1fc71369c2464127e51381f4a1f16
Dong Ju <dj.chung@xiilab.com>
2022-08-18 12:20:04 +0900

    (임시)login-function

---
+++ blog/src/main/resources/static/js/user.js
@@ -0,0 +1,40 @@
+let index = {
+    init: function () {
+        //let _this = this; function을 사용할 때 선언
+        //첫번쨰 파라메터에는 어떤 이벤트 두번째 파라메터에는 무엇을 할꺼야?
+        $("#btn-save").on("click", () => { //function(){}을 사용하지 않고 ()=>{}을 사용하는 이유는 this를 바인딩하기 위해
+            this.save();
+        });
+    },
+
+    save: function () {
+        //alert("user의 save함수 호출됨");
+        let data = {
+            username:$("#username").val(),
+            password:$("#password").val(),
+            email:$("#email").val()
+        };
+
+        //console.log(data);
+
+        //ajax호출 시 default가 비동기 호출
+        //ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!
+        $.ajax({
+            //회원가입 수행 요청(100초 가정)
+            type:"POST",
+            url:"/blog/api/user",
+            data: JSON.stringify(data), //http body데이터
+            contentType: "application/json;",
+            dataType: "json" //요청을 서버로 응답이 왔을 떄 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript오브젝트로 변경
+        }).done(function(resp) {
+            alert("회원가입이 완료되었습니다.");
+            console.log(resp);
+            alert(resp);
+            //location.href="/blog"
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+}
+
+index.init();
