76fe56dd1adf629881a07c876bc8ff76cf54f248
Dong Ju <dj.chung@xiilab.com>
2022-08-18 15:22:39 +0900

    join-function(service클래스추가)

---
+++ blog/src/main/java/com/cos/blog/service/UserService.java
@@ -0,0 +1,23 @@
+package com.cos.blog.service;
+
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Slf4j
+//스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC를 해준다
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public void join(User user) {
+        userRepository.save(user);
+    }
+}
